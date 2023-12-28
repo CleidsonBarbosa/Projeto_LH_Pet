c3a4886ed43d488d82210f426b760a816718eadd
Cleidson.Barbosa <cleidson-barbosa@hotmail.com>
2023-12-27 23:23:16 -0300

    Projeto LH Pet

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Projeto_Web_Lh_Pets_versão_1;
+
+namespace Projeto_LH_Pet;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto Web - LH Pets - Versão 1");
+
+        app.UseStaticFiles();
+
+        app.MapGet("/index",(HttpContext contexto)=>{
+            contexto.Response.Redirect("index.html", false);
+        });
+
+        Banco dba=new Banco();
+
+        app.MapGet("/listClientes", (HttpContext contexto)=>{
+
+            contexto.Response.WriteAsync(dba.GetListaString());
+
+        });
+
+        app.Run();
+    }
+}
